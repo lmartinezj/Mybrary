9f3e32f26d20f919426910439a122f0754c73450
lmartinez <lmartinezj@gmail.com>
2021-05-05 00:18:16 -0500

    Finish project

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,16 +1,31 @@
-// Plugins can be registered with FilePond using the registerPlugin method
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode
-)
+const rootStyles = window.getComputedStyle(document.documentElement)
 
-// Set aspect ratio for the drop area to make the drop area take the same space as the book cover.
-FilePond.setOptions({ 
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150
-})
+if (rootStyles.getPropertyValue('--book-cover-width-large') != null && 
+rootStyles.getPropertyValue('--book-cover-width-large') != '') {
+    ready()
+} else {
+    document.getElementById('main-css').addEventListener('load', ready)
+}
 
-//Turn all file input elements into ponds
-FilePond.parse(document.body);
+function ready() {
+    const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'))
+    const coverAspectRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio'))
+    const coverHeight = coverWidth / coverAspectRatio
+
+    // Plugins can be registered with FilePond using the registerPlugin method
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode
+    )
+
+    // Set aspect ratio for the drop area to make the drop area take the same space as the book cover.
+    FilePond.setOptions({ 
+        stylePanelAspectRatio: 1 / coverAspectRatio,
+        imageResizeTargetWidth: coverWidth,
+        imageResizeTargetHeight: coverHeight
+    })
+
+    //Turn all file input elements into ponds
+    FilePond.parse(document.body);
+}
